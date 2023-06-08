395debee1320d06168973249f870568cd6a9c7c5
sjpi22 <124000054+sjpi22@users.noreply.github.com>
2023-06-07 23:40:50 -0700

    authorship

--- train_multiple_relationships.py
+++ train_multiple_relationships.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# DRKG
-# 
-# Adapted from: https://github.com/gnn4dr/DRKG/blob/master/drkg_with_dgl/loading_drkg_in_dgl.ipynb
+
+# AUTHORS: Adapted from train_multiple_relationships.ipynb by Alejandro and Selina, with minor edits by Rohan to run on Google Cloud cluster and save outputs
 
 # In[1]:
 
